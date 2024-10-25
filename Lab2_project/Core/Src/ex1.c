b4ba8d50c2a450da7935b6f2c1abb414faafb465
Tien CSE <tien.nguyen11102004@hcmut.edu.vn>
2024-10-25 18:36:49 +0700

    change sow

--- Lab2_project/Core/Src/ex1.c
+++ Lab2_project/Core/Src/ex1.c
@@ -12,12 +12,12 @@ void ex1_init(){
 	HAL_GPIO_WritePin(GPIOA,EN1_Pin, RESET);
 	HAL_GPIO_WritePin(GPIOA,EN2_Pin, SET);
 	DISPLAY_LED_7_SEGMENT(GPIOB, SEG0_Pin, SEG1_Pin, SEG2_Pin, SEG3_Pin, SEG4_Pin, SEG5_Pin, SEG6_Pin, num);
-	setTimer1(0,100);
+	setTimer1(0,1000);
 }
 
 void ex1_run(){
 	if(timer1_flag[0]==1){
-		setTimer1(0,100);
+		setTimer1(0,1000);
 		num=3-num;
 		HAL_GPIO_TogglePin(GPIOA, LED_RED_Pin);
 		HAL_GPIO_TogglePin(GPIOA, EN1_Pin);
